50c7b530901edf1820df49fe6579ed9c2075597d
Paulo Cauan Lima Pereira <paulocauan39@gmail.com>
2025-08-16 00:27:32 +0000

    .

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-alert("Meu pastel é mais barato")
-console.log("Palmeiras não tem mundial")
-console.log("Corinthians maior time do mundo")
-
-let valor_qualquer
-valor_qualquer = 1+1
-alert:valor_qualquer
+let valor_qualquer;
+valor_qualquer = 1+1;
+alert:valor_qualquer;
